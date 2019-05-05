06a271f79fe069aaf3fb98ca237ed125a97baa98
Nikhil Kshirsagar <nkjava2050@gmail.com>
2019-05-06 04:34:25 +0530

    Added functionality to parse multiple pages

--- MainArchitect.py
+++ MainArchitect.py
@@ -3,6 +3,8 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
+content = []
+
 def simple_get(url):
     """
     Attempts to get the content at `url` by making an HTTP GET request.
@@ -41,38 +43,44 @@ def log_error(e):
     """
     print(e)
     
-raw_html = simple_get('https://find-an-architect.architecture.com/FAAPractices.aspx')
-html = BeautifulSoup(raw_html, 'html.parser')
+def scrap_html(sectionTag) :
+    #print(sectionTag.findAll("article"))
+    for myItem in sectionTag.findAll("article"):
+        info = {}
+        info["name"] = myItem.find('a').string
+        
+        if myItem.find('div', {"class":"address"}) is not None:
+            info["addr"] = myItem.find('div', {"class":"address"}).string.strip()
+            addrPart = myItem.find('div', {"class":"address"}).string.strip().split(",")
+            #print(addrPart[len(addrPart)-2],"\t",addrPart[len(addrPart)-1])
+            info["zip"] = addrPart[len(addrPart)-1]
+            info["city"] = addrPart[len(addrPart)-2]
 
-#print(html.find(class="listColumn-withSearch"))# select().prettify())
-#print(html.select("section")[0].prettify())
-sectionTag = html.findAll("section",{"class":"listingColumn-withSearch","class":"listingColumn", "aria-labelledby":"sectionHeading"})[0]
-#print(sectionTag.prettify())
+        for pageMetaDiv in myItem.findAll('div', {"class":"pageMeta-item"}):
+            if str(pageMetaDiv.string).startswith('Tel: ') and pageMetaDiv.string is not None:
+                #print("CHILD[",str(i),"] :: ",str(pageMetaDiv.string).split(':')[1].strip())
+                info["phone"] = str(pageMetaDiv.string).split(':')[1].strip()
+            elif pageMetaDiv.find("a", {"class":"tagList","class":"faaemail"}):
+                #print("pageMetaDiv :: ", pageMetaDiv.find("a", {"class":"tagList","class":"faaemail"}).string)
+                info["email"] = pageMetaDiv.find("a", {"class":"tagList","class":"faaemail"}).string
+            elif pageMetaDiv.find("a", {"class":"tagList","class":"exLink"}):
+                #print("pageMetaDiv :: ", pageMetaDiv.find("a", {"class":"tagList","class":"exLink"}).string)
+                info["website"] = pageMetaDiv.find("a", {"class":"tagList","class":"exLink"}).string        
+        content.append(info)
 
-#print(sectionTag.findAll("article"))
+    #print("Content :: ", content)
+    #print("--------------------------------------------------------------------------\n")
+    return content
+
 content = []
-#print(sectionTag.findAll("article"))
-for myItem in sectionTag.findAll("article"):
-    info = {}
-    info["name"] = myItem.find('a').string
-    info["addr"] = myItem.find('div', {"class":"address"}).string.strip()
-    
-    addrPart = myItem.find('div', {"class":"address"}).string.strip().split(",")
-    #print(addrPart[len(addrPart)-2],"\t",addrPart[len(addrPart)-1])
-    info["zip"] = addrPart[len(addrPart)-1]
-    info["city"] = addrPart[len(addrPart)-2]
+for pageNo in range(0,77):
+    print("Reached Page ::", pageNo)
+    raw_html = simple_get('https://find-an-architect.architecture.com/FAAPractices.aspx?page=%s'%(pageNo))
+    html = BeautifulSoup(raw_html, 'html.parser')
 
-    for pageMetaDiv in myItem.findAll('div', {"class":"pageMeta-item"}):
-        if str(pageMetaDiv.string).startswith('Tel: ') and pageMetaDiv.string is not None:
-            #print("CHILD[",str(i),"] :: ",str(pageMetaDiv.string).split(':')[1].strip())
-            info["phone"] = str(pageMetaDiv.string).split(':')[1].strip()
-        elif pageMetaDiv.find("a", {"class":"tagList","class":"faaemail"}):
-            #print("pageMetaDiv :: ", pageMetaDiv.find("a", {"class":"tagList","class":"faaemail"}).string)
-            info["email"] = pageMetaDiv.find("a", {"class":"tagList","class":"faaemail"}).string
-        elif pageMetaDiv.find("a", {"class":"tagList","class":"exLink"}):
-            #print("pageMetaDiv :: ", pageMetaDiv.find("a", {"class":"tagList","class":"exLink"}).string)
-            info["website"] = pageMetaDiv.find("a", {"class":"tagList","class":"exLink"}).string        
-    content.append(info)
-    
-print("Content :: ", content)
-
+    #print(html.find(class="listColumn-withSearch"))# select().prettify())
+    #print(html.select("section")[0].prettify())
+    sectionTag = html.findAll("section",{"class":"listingColumn-withSearch","class":"listingColumn", "aria-labelledby":"sectionHeading"})[0]
+    # print(sectionTag.prettify())
+    scrap_html(sectionTag)
+print(content)
